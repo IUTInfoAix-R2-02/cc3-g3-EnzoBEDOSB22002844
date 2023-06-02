a741687d4cb7225335d85f9f2162671dbe0fb4b3
EnzoBEDOS22002844 <enzo.bedos@etu.univ-amu.fr>
2023-06-02 15:58:48 +0200

    Placement point terminé et finalisé

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -12,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
@@ -32,9 +33,9 @@ public class ToileController implements Initializable {
     private static int angleDepart = 90;
     private static int noteMaximale = 20;
 
-    @FXML
-    private Shape pointtest;
 
+    @FXML
+    private Label labelError;
     @FXML
     private Shape point1;
     @FXML
@@ -102,29 +103,54 @@ public class ToileController implements Initializable {
     }
     @FXML
     private void writeComp(){
-        int x1 = getXRadarChart(Integer.valueOf(FieldC1.getText()),1);
-        int y1 = getYRadarChart(Integer.valueOf(FieldC1.getText()),1);
-        setPoint1(point1, x1, y1);
+        if (Integer.valueOf(FieldC1.getText())> 20 ||Integer.valueOf(FieldC1.getText()) < 0 ){
+            labelError.setVisible(true);
+        } else {
+            int x1 = getXRadarChart(Integer.valueOf(FieldC1.getText()),1);
+            int y1 = getYRadarChart(Integer.valueOf(FieldC1.getText()),1);
+            setPoint1(point1, x1, y1);
+        }
 
-        int x2 = getXRadarChart(Integer.valueOf(FieldC2.getText()), 2);
-        int y2 = getYRadarChart(Integer.valueOf(FieldC2.getText()),2);
-        setPoint2(point2, x2, y2);
+        if (Integer.valueOf(FieldC2.getText())> 20 ||Integer.valueOf(FieldC2.getText()) < 0 ){
+            labelError.setVisible(true);
+        } else {
+            int x2 = getXRadarChart(Integer.valueOf(FieldC2.getText()), 2);
+            int y2 = getYRadarChart(Integer.valueOf(FieldC2.getText()),2);
+            setPoint2(point2, x2, y2);
+        }
 
-        int x3 = getXRadarChart(Integer.valueOf(FieldC3.getText()),3);
-        int y3 = getYRadarChart(Integer.valueOf(FieldC3.getText()),3);
-        setPoint3(point3, x3, y3);
+        if (Integer.valueOf(FieldC3.getText())> 20 ||Integer.valueOf(FieldC3.getText()) < 0 ){
+            labelError.setVisible(true);
+        } else {
+            int x3 = getXRadarChart(Integer.valueOf(FieldC3.getText()),3);
+            int y3 = getYRadarChart(Integer.valueOf(FieldC3.getText()),3);
+            setPoint3(point3, x3, y3);
+        }
 
-        int x4 = getXRadarChart(Integer.valueOf(FieldC4.getText()),4);
-        int y4 = getYRadarChart(Integer.valueOf(FieldC4.getText()),4);
-        setPoint4(point4, x4, y4);
+        if (Integer.valueOf(FieldC4.getText())> 20 ||Integer.valueOf(FieldC4.getText()) < 0 ){
+            labelError.setVisible(true);
+        } else {
+            int x4 = getXRadarChart(Integer.valueOf(FieldC4.getText()),4);
+            int y4 = getYRadarChart(Integer.valueOf(FieldC4.getText()),4);
+            setPoint4(point4, x4, y4);
+            
+        }
 
-        int x5 = getXRadarChart(Integer.valueOf(FieldC5.getText()),5);
-        int y5 = getYRadarChart(Integer.valueOf(FieldC5.getText()),5);
-        setPoint5(point5, x5, y5);
+        if (Integer.valueOf(FieldC5.getText())> 20 ||Integer.valueOf(FieldC5.getText()) < 0 ){
+            labelError.setVisible(true);
+        } else {
+            int x5 = getXRadarChart(Integer.valueOf(FieldC5.getText()),5);
+            int y5 = getYRadarChart(Integer.valueOf(FieldC5.getText()),5);
+            setPoint5(point5, x5, y5);
+        }
 
-        int x6 = getXRadarChart(Integer.valueOf(FieldC6.getText()),6);
-        int y6 = getYRadarChart(Integer.valueOf(FieldC6.getText()),6);
-        setPoint6(point6, x6, y6);
+        if (Integer.valueOf(FieldC6.getText())> 20 ||Integer.valueOf(FieldC6.getText()) < 0 ){
+            labelError.setVisible(true);
+        } else {
+            int x6 = getXRadarChart(Integer.valueOf(FieldC6.getText()),6);
+            int y6 = getYRadarChart(Integer.valueOf(FieldC6.getText()),6);
+            setPoint6(point6, x6, y6);
+        }
     }
     @FXML
     private void clearField(){
@@ -140,6 +166,7 @@ public class ToileController implements Initializable {
         setPoint4(point4, 200, 200);
         setPoint5(point5, 200, 200);
         setPoint6(point6, 200, 200);
+        labelError.setVisible(false);
     }
 
 
